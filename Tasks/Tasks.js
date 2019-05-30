5f3583e0cf1ab528066105c7af9c766501081625
Thomas Øren <42614221+thomasoren@users.noreply.github.com>
2019-05-30 22:15:08 +0200

    Laget openSlide function

--- Tasks/Tasks.js
+++ Tasks/Tasks.js
@@ -263,3 +263,8 @@ $(function() {
 
     })
 })
+
+function openSlideMenu(){
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('content').style.marginLeft = '250px';
+}

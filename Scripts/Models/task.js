e37bf63c5d502527fd308f1797412df41a2aa453
Julie Mamelund <julie@mamelund.no>
2019-06-01 16:21:56 +0200

    made a status checker for Task status

--- Scripts/Models/task.js
+++ Scripts/Models/task.js
@@ -10,9 +10,22 @@ export class Task {
         this.name = name
         this.description = description
         this.members = new Array(member)
+        this.status = 'todo'
     }
 
-
+    setStatus = function(status){
+        switch(status){
+            case 'todo':
+            case 'ongoing':
+            case 'done':
+                // Fall-through intended
+                // Valid value. Continue.
+                break
+            default:
+                throw 'Use todo, ongoing or done for Task status'
+        }
+        this.status = status
+    }
 
     printName = function() {
          console.log(this.name)
--- Scripts/test.js
+++ Scripts/test.js
@@ -3,25 +3,42 @@ import {TaskService} from './Services/task_service.js'
 import {MemberService} from './Services/member_service.js'
 import { Member } from './Models/member.js';
 
-let member1 = new Member(10, 'jumam', 'julie Heisann')
 
-let memberService = new MemberService()
-memberService.addMember(member1)
 
-let member = memberService.getByUsername('jumam')
+testSetTaskStatus()
+//testMembersAndTasks()
 
-if(member === undefined) {
-    console.log('Fant ikke medlem!')
-}
-else {
-    let task1 = new Task('Clean bathroom', 'Clean the bathroom with soap', member1)
-    let task2 = new Task('Walk the dog', 'Take Spot for a walk around the block', member)
 
-    let taskService = new TaskService()
-    taskService.addTask(task1)
-    taskService.addTask(task2)
+function testSetTaskStatus(){
 
-    console.log(taskService.count())
-    console.log(task1.members[0].name)
+    let task = new Task('Task 1', 'This is the first task', null)
+    task.setStatus('todo')
+    console.log(task.status)
 }
 
+
+function testMembersAndTasks(){
+
+    let member1 = new Member(10, 'jumam', 'julie Heisann')
+
+    let memberService = new MemberService()
+    memberService.addMember(member1)
+
+    let member = memberService.getByUsername('jumam')
+
+    if(member === undefined) {
+        console.log('Fant ikke medlem!')
+    }
+    else {
+        let task1 = new Task('Clean bathroom', 'Clean the bathroom with soap', member1)
+        let task2 = new Task('Walk the dog', 'Take Spot for a walk around the block', member)
+
+        let taskService = new TaskService()
+        taskService.addTask(task1)
+        taskService.addTask(task2)
+
+        console.log(taskService.count())
+        console.log(task1.members[0].name)
+    }
+
+}

3b78635589e69f170c24e4a8c9f021aa8abef4e5
Julie Mamelund <julie@mamelund.no>
2019-06-02 13:24:18 +0200

    finished restructuring of Tasks.js

--- Tasks/Tasks.js
+++ Tasks/Tasks.js
@@ -5,82 +5,88 @@ $(document).ready(function() {
 
     let app = new AppManager()
     app.loadData()
+
     wireUpEvents()
     createDialogs()
+    initDragDrop()
+    initSortable()
+    displayData(app)
 
-    // Defines that the div's with a specific id is made sortable and connects sortable on all fields. 
-    $(function() {
-        $("#backlog-list, #to-do-list, #doing-list, #done-list").sortable({
-            connectWith: ".connectedSortable"
-        }).disableSelection();
-    })
-
-
-    //Makes the specified id's droppable, and removes class and adds class on dragged.
-    $(function() {
-            $("#to-do-list, #doing-list, #backlog-list").droppable({
-                drop: function(event, ui) {
-                    ui.draggable.removeClass("done-task").addClass("task-cards")
-                }
-            })
-        })
-        //Making the done list droppable, and removes and adds class on dropped elements.
-    $(function() {
-        $("#done-list").droppable({
-            drop: function(event, ui) {
-                ui.draggable.addClass("done-task").removeClass("task-cards")
-
-            }
-        })
-    })
-
-    //function for cleaning inputfields. Just sets all the values in a modal to empty.
-    function cleanInput() {
-        $("#input-task-name").val("")
-        $("#input-task-desc").val("")
-        $("#input-task-point").val("")
-        $("#task-modal").dialog("close")
-    }
-
-    //function for creating cards, with value and it creates a HTML template that is appended by a list.
-    function createCard() {
-        let taskText = $("#input-task-name").val()
-        let taskDesc = $("#input-task-desc").val()
-        let taskPoint = $("#input-task-point").val()
-
-        $("#backlog-list").append(`
-        <li class="task-cards">
-        <article>
-        <p class="delete-task-button">x</p>
-        <p class="edit-button">edit</p>
-
-            <h3 class="task-text">Taskname: ${taskText}</h3>
-            <p class="task-point">User: ${taskPoint}</p>
-            <p class="read-more">Details</p>
-            <p class="task-desc">Description: ${taskDesc}</p>
-            
-        </article>
-    </li>`)
-    }
-
-    $(function(){
-        let tasks = app.taskService.getTasksByStatus('todo')
-        for(let task of tasks){
-            // Fake input of form, since createCard() uses form data and not parameters
-            $("#input-task-name").val(task.name);
-            $("#input-task-desc").val(task.description);
-            $("#input-task-point").val(task.members[0].name);
-            createCard();
-        }
-    });
-    
-    
 });
 
+
+//Functions
+/***
+ *      █████▒█    ██  ███▄    █  ▄████▄  ▄▄▄█████▓ ██▓ ▒█████   ███▄    █   ██████ 
+ *    ▓██   ▒ ██  ▓██▒ ██ ▀█   █ ▒██▀ ▀█  ▓  ██▒ ▓▒▓██▒▒██▒  ██▒ ██ ▀█   █ ▒██    ▒ 
+ *    ▒████ ░▓██  ▒██░▓██  ▀█ ██▒▒▓█    ▄ ▒ ▓██░ ▒░▒██▒▒██░  ██▒▓██  ▀█ ██▒░ ▓██▄   
+ *    ░▓█▒  ░▓▓█  ░██░▓██▒  ▐▌██▒▒▓▓▄ ▄██▒░ ▓██▓ ░ ░██░▒██   ██░▓██▒  ▐▌██▒  ▒   ██▒
+ *    ░▒█░   ▒▒█████▓ ▒██░   ▓██░▒ ▓███▀ ░  ▒██▒ ░ ░██░░ ████▓▒░▒██░   ▓██░▒██████▒▒
+ *     ▒ ░   ░▒▓▒ ▒ ▒ ░ ▒░   ▒ ▒ ░ ░▒ ▒  ░  ▒ ░░   ░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ▒ ▒▓▒ ▒ ░
+ *     ░     ░░▒░ ░ ░ ░ ░░   ░ ▒░  ░  ▒       ░     ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░░ ░▒  ░ ░
+ *     ░ ░    ░░░ ░ ░    ░   ░ ░ ░          ░       ▒ ░░ ░ ░ ▒     ░   ░ ░ ░  ░  ░  
+ *              ░              ░ ░ ░                ░      ░ ░           ░       ░  
+ *                               ░                                                  
+ */
+
+function displayData(app){
+    let tasks = app.taskService.getTasksByStatus('todo')
+    for(let task of tasks){
+        // Fake input of form, since createCard() uses form data and not parameters
+        $("#input-task-name").val(task.name);
+        $("#input-task-desc").val(task.description);
+        $("#input-task-point").val(task.members[0].name);
+        createCard();
+    }
+}
 
 function toggleSidebar() {
     document.body.classList.toggle('sidenav-active');
 }
+
+//function for cleaning inputfields. Just sets all the values in a modal to empty.
+function cleanInput() {
+    $("#input-task-name").val("")
+    $("#input-task-desc").val("")
+    $("#input-task-point").val("")
+    $("#task-modal").dialog("close")
+}
+
+//function for creating cards, with value and it creates a HTML template that is appended by a list.
+function createCard() {
+    let taskText = $("#input-task-name").val()
+    let taskDesc = $("#input-task-desc").val()
+    let taskPoint = $("#input-task-point").val()
+
+    $("#backlog-list").append(`
+    <li class="task-cards">
+    <article>
+    <p class="delete-task-button">x</p>
+    <p class="edit-button">edit</p>
+
+        <h3 class="task-text">Taskname: ${taskText}</h3>
+        <p class="task-point">User: ${taskPoint}</p>
+        <p class="read-more">Details</p>
+        <p class="task-desc">Description: ${taskDesc}</p>
+        
+    </article>
+</li>`)
+}
+
+
+//Initial page setup
+/***
+ *     ██▓ ███▄    █  ██▓▄▄▄█████▓ ██▓ ▄▄▄       ██▓        ██▓███   ▄▄▄        ▄████ ▓█████      ██████ ▓█████▄▄▄█████▓ █    ██  ██▓███  
+ *    ▓██▒ ██ ▀█   █ ▓██▒▓  ██▒ ▓▒▓██▒▒████▄    ▓██▒       ▓██░  ██▒▒████▄     ██▒ ▀█▒▓█   ▀    ▒██    ▒ ▓█   ▀▓  ██▒ ▓▒ ██  ▓██▒▓██░  ██▒
+ *    ▒██▒▓██  ▀█ ██▒▒██▒▒ ▓██░ ▒░▒██▒▒██  ▀█▄  ▒██░       ▓██░ ██▓▒▒██  ▀█▄  ▒██░▄▄▄░▒███      ░ ▓██▄   ▒███  ▒ ▓██░ ▒░▓██  ▒██░▓██░ ██▓▒
+ *    ░██░▓██▒  ▐▌██▒░██░░ ▓██▓ ░ ░██░░██▄▄▄▄██ ▒██░       ▒██▄█▓▒ ▒░██▄▄▄▄██ ░▓█  ██▓▒▓█  ▄      ▒   ██▒▒▓█  ▄░ ▓██▓ ░ ▓▓█  ░██░▒██▄█▓▒ ▒
+ *    ░██░▒██░   ▓██░░██░  ▒██▒ ░ ░██░ ▓█   ▓██▒░██████▒   ▒██▒ ░  ░ ▓█   ▓██▒░▒▓███▀▒░▒████▒   ▒██████▒▒░▒████▒ ▒██▒ ░ ▒▒█████▓ ▒██▒ ░  ░
+ *    ░▓  ░ ▒░   ▒ ▒ ░▓    ▒ ░░   ░▓   ▒▒   ▓▒█░░ ▒░▓  ░   ▒▓▒░ ░  ░ ▒▒   ▓▒█░ ░▒   ▒ ░░ ▒░ ░   ▒ ▒▓▒ ▒ ░░░ ▒░ ░ ▒ ░░   ░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░
+ *     ▒ ░░ ░░   ░ ▒░ ▒ ░    ░     ▒ ░  ▒   ▒▒ ░░ ░ ▒  ░   ░▒ ░       ▒   ▒▒ ░  ░   ░  ░ ░  ░   ░ ░▒  ░ ░ ░ ░  ░   ░    ░░▒░ ░ ░ ░▒ ░     
+ *     ▒ ░   ░   ░ ░  ▒ ░  ░       ▒ ░  ░   ▒     ░ ░      ░░         ░   ▒   ░ ░   ░    ░      ░  ░  ░     ░    ░       ░░░ ░ ░ ░░       
+ *     ░           ░  ░            ░        ░  ░    ░  ░                  ░  ░      ░    ░  ░         ░     ░  ░           ░              
+ *                                                                                                                                        
+ */
 
 function wireUpEvents(){
 
@@ -137,7 +143,6 @@ function wireUpEvents(){
     });
 }
 
-
 function createDialogs(){
 
     $("#startup-dialog").dialog({
@@ -168,4 +173,29 @@ function createDialogs(){
             effect: "fade"
         }
     })
+}
+
+function initDragDrop(){
+
+    //Makes the specified id's droppable, and removes class and adds class on dragged.
+    $("#to-do-list, #doing-list, #backlog-list").droppable({
+        drop: function(event, ui) {
+            ui.draggable.removeClass("done-task").addClass("task-cards")
+        }
+    })
+
+    //Making the done list droppable, and removes and adds class on dropped elements.
+    $("#done-list").droppable({
+        drop: function(event, ui) {
+            ui.draggable.addClass("done-task").removeClass("task-cards")
+        }
+    })
+}
+
+function initSortable(){
+
+    // Defines that the div's with a specific id is made sortable and connects sortable on all fields. 
+    $("#backlog-list, #to-do-list, #doing-list, #done-list").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
 }
